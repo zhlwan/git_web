a65c6ba055f334996a79e2ff1df6533708097d23
zhl <811105078@qq.com>
2018-04-28 17:58:59 +0800

    赵洪磊的第一次提交

--- src/java/com/baizhi/controller/StudentController.java
+++ src/java/com/baizhi/controller/StudentController.java
@@ -8,7 +8,7 @@ public class StudentController {
     public static void main(String[] args) {
         System.out.println("第一次修改");
         System.out.println("第二次修改");
-
+        System.out.println("赵洪磊");
 
     }
 }
