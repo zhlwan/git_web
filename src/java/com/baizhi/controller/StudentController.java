0d01aabe2dfbe9be205fcb4c3fe775ba575ce400
zhl <811105078@qq.com>
2018-04-28 16:44:12 +0800

    第一次修改

---
+++ src/java/com/baizhi/controller/StudentController.java
@@ -0,0 +1,11 @@
+package com.baizhi.controller;
+
+/**
+ * Created by Administrator on 2018/4/28.
+ */
+public class StudentController {
+
+    public static void main(String[] args) {
+        System.out.println("第一次修改");
+    }
+}
